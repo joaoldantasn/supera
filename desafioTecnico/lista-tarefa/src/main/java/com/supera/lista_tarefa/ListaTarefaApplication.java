d17ae70be34d2caea50bd78814ba33cbdf4d2288
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-08-22 08:27:56 -0300

    configuracoes h2

---
+++ desafioTecnico/lista-tarefa/src/main/java/com/supera/lista_tarefa/ListaTarefaApplication.java
@@ -0,0 +1,13 @@
+package com.supera.lista_tarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaTarefaApplication.class, args);
+	}
+
+}
---
+++ desafioTecnico/lista-tarefa/src/test/java/com/supera/lista_tarefa/ListaTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supera.lista_tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
